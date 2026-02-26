88fd585e36f19f2aedf5817db309135a10a1fca0
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-26 16:40:12 +0100

    File updated at 2/26/2026, 4:40:12 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,12 +1,20 @@
-# Last updated: 2/26/2026, 4:21:51 PM
-class Solution(object):
-    def majorityElement(self, nums):
-        d={}
-        for i in nums:
-            if i not in d.keys():
-                d[i]=1
-            else:
-                d[i]+=1
-        return max(d,key=d.get)
-
-        
+# Last updated: 2/26/2026, 4:40:12 PM
+1class Solution(object):
+2    def rotate(self, nums, k):
+3        """
+4        :type nums: List[int]
+5        :type k: int
+6        :rtype: None Do not return anything, modify nums in-place instead.
+7        """
+8        n = len(nums)
+9
+10        k %= n 
+11
+12        r = n - k
+13
+14        new = nums[0:r]
+15
+16        nums[0:r] = []
+17
+18        nums.extend(new)
+19        
