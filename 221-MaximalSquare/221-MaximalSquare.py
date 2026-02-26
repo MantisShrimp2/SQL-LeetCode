edf89ebe4ea66346d048fe91216fa9786a5f2afa
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-26 14:11:41 +0100

    File updated at 2/26/2026, 2:11:41 PM

--- 221-MaximalSquare/221-MaximalSquare.py
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -1,14 +1,19 @@
-# Last updated: 2/26/2026, 2:01:04 PM
+# Last updated: 2/26/2026, 2:11:41 PM
 1class Solution(object):
-2    def removeElement(self, nums, val):
+2    def removeDuplicates(self, nums):
 3        """
 4        :type nums: List[int]
-5        :type val: int
-6        :rtype: int
-7        """
-8        k = 0
-9        for n in nums:
-10            if n != val:
-11                nums[k] = n
-12                k +=1
-13        return k
+5        :rtype: int
+6        """
+7        
+8        seen = {}
+9        k = 0
+10
+11        for i in range(len(nums)):
+12            if nums[i] not in seen:
+13                seen[nums[i]] = True
+14                nums[k] = nums[i]
+15                k += 1
+16
+17        return k
+18        
