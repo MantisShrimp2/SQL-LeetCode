7a3b609c7e680043809c57b2548cd7de3d3c8a98
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-26 17:49:03 +0100

    File updated at 2/26/2026, 5:49:03 PM

--- 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
+++ 1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne/1404-NumberofStepstoReduceaNumberinBinaryRepresentationtoOne.py
@@ -1,18 +1,18 @@
-# Last updated: 2/26/2026, 5:48:32 PM
-1class Solution(object):
-2    def numSteps(self, s):
-3        """
-4        :type s: str
-5        :rtype: int
-6        """
-7        base = int(s,2)
-8        steps = 0
-9
-10        while base !=1:
-11            if base % 2 == 0:
-12                base = base//2
-13                steps +=1
-14            else:
-15                base +=1
-16                steps +=1
-17        return steps
+# Last updated: 2/26/2026, 5:49:03 PM
+class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        c=0
+        
+        num=int(s,2)
+        while(num!=1):
+            if((num&1)==1):
+                num+=1
+                c+=1
+            else:
+                num=num>>1
+                c+=1
+        return c
