a284dd771d4d9af052921a0a8b6c38b2f9cab715
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-26 16:21:51 +0100

    File updated at 2/26/2026, 4:21:51 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,18 +1,12 @@
-# Last updated: 2/26/2026, 4:20:12 PM
-1class Solution(object):
-2    def majorityElement(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: int
-6        """
-7
-8        maj_dict = {}
-9
-10        for n in nums:
-11            if n not in maj_dict:
-12                maj_dict[n] =1
-13            else:
-14                maj_dict[n] +=1
-15        max_key = max(maj_dict, key=maj_dict.get)
-16        return max_key
-17        
+# Last updated: 2/26/2026, 4:21:51 PM
+class Solution(object):
+    def majorityElement(self, nums):
+        d={}
+        for i in nums:
+            if i not in d.keys():
+                d[i]=1
+            else:
+                d[i]+=1
+        return max(d,key=d.get)
+
+        
