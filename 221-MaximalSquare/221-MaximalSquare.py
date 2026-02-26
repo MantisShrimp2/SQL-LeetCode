d08fde1a73ba9cb7f9cb0922566709745f2f3ddb
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-26 14:01:04 +0100

    File updated at 2/26/2026, 2:01:04 PM

--- 221-MaximalSquare/221-MaximalSquare.py
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -1,14 +1,14 @@
-# Last updated: 2/26/2026, 1:54:58 PM
+# Last updated: 2/26/2026, 2:01:04 PM
 1class Solution(object):
-2    def merge(self, nums1, m, nums2, n):
+2    def removeElement(self, nums, val):
 3        """
-4        :type nums1: List[int]
-5        :type m: int
-6        :type nums2: List[int]
-7        :type n: int
-8        :rtype: None Do not return anything, modify nums1 in-place instead.
-9        """
-10        for j in range(n):
-11            nums1[m+j] = nums2[j]
-12        nums1.sort()
-13        
+4        :type nums: List[int]
+5        :type val: int
+6        :rtype: int
+7        """
+8        k = 0
+9        for n in nums:
+10            if n != val:
+11                nums[k] = n
+12                k +=1
+13        return k
