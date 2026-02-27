fb42cb56bb9aaf6d70d0019d4b6d9fd3233a5de7
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-27 16:27:35 +0100

    File updated at 2/27/2026, 4:27:35 PM

--- 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
+++ 80-RemoveDuplicatesfromSortedArrayII/80-RemoveDuplicatesfromSortedArrayII.py
@@ -1,21 +1,16 @@
-# Last updated: 2/27/2026, 3:58:47 PM
+# Last updated: 2/27/2026, 4:27:35 PM
 1class Solution(object):
-2    def removeDuplicates(self, nums):
+2    def canJump(self, nums):
 3        """
 4        :type nums: List[int]
-5        :rtype: int
+5        :rtype: bool
 6        """
-7
-8        count =0
-9        current = 1
-10        for i in range(1, len(nums)):
-11            if nums[i] != nums[i-1]:
-12                count = 0
-13                nums[current] = nums[i]
-14                current +=1
-15            else:
-16                count +=1
-17                if count <=1:
-18                    nums[current] = nums[i]
-19                    current +=1
-20        return current
+7        m = 0 
+8        for i ,n in enumerate(nums):
+9 
+10            if i > m: 
+11                return False
+12            m = max(m, i+n)
+13
+14        return True
+15        
