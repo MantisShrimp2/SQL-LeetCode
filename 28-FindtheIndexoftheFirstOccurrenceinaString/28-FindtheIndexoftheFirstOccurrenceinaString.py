4f26c3bb209b3984a66709ae3e1b0d83ef965089
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-27 16:49:41 +0100

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
@@ -0,0 +1,9 @@
+# Last updated: 2/27/2026, 4:49:40 PM
+1class Solution(object):
+2    def strStr(self, haystack, needle):
+3        """
+4        :type haystack: str
+5        :type needle: str
+6        :rtype: int
+7        """
+8        return haystack.find(needle)
