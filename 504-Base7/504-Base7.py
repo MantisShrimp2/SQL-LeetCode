950ebc9ffe7120ab53343818df32ec150042b68b
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-28 18:04:30 +0100

    Create 504-Base7

---
+++ 504-Base7/504-Base7.py
@@ -0,0 +1,21 @@
+# Last updated: 2/28/2026, 6:04:30 PM
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num== 0:
+            return "0"
+        negative = num <0
+        num = abs(num)
+        result = ''
+        while num > 0:
+            result = str(num % 7) + result
+            num //=7
+        
+        return ("-" if negative else "") + result
+        
+        
+            
+        
