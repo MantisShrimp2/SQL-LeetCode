c9bb1fac4e70877c54f050a94bde2218c621aefe
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-02-28 12:50:19 +0100

    File updated at 2/28/2026, 12:50:19 PM

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,16 +1,17 @@
-# Last updated: 2/27/2026, 9:06:30 PM
-class Solution(object):
-    def canJump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        m = 0 
-        for i ,n in enumerate(nums):
- 
-            if i > m: 
-                return False
-            m = max(m, i+n)
-
-        return True
-        
+# Last updated: 2/28/2026, 12:50:19 PM
+1class Solution(object):
+2    def lengthOfLastWord(self, s):
+3        """
+4        :type s: str
+5        :rtype: int
+6        """
+7
+8        s_len = len(s)
+9        word = []
+10        for i in range(s_len-1, -1, -1):   # 4, 3, 2, 1, 0
+11            if s[i] != ' ':
+12                word.append(s[i])
+13            elif word:
+14                break
+15        return len(word)
+16        
