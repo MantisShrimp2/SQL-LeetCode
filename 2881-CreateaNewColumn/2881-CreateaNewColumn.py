332e60b63c1eb02c6f48db5a6e135ed536d9110f
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:35:07 +0100

    File updated at 3/1/2026, 10:35:07 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,6 +1,6 @@
-# Last updated: 3/1/2026, 10:33:29 AM
-import pandas as pd
-
-def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students['grade'] = students['grade'].astype(int)
-    return students
+# Last updated: 3/1/2026, 10:35:07 AM
+1import pandas as pd
+2
+3def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+4    products['quantity'] = products['quantity'].fillna(0)
+5    return products
