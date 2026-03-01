e52ea07904678d3707b09ef8ac53f7d478e4abad
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:12:33 +0100

    File updated at 3/1/2026, 10:12:33 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,6 +1,5 @@
-# Last updated: 3/1/2026, 10:12:04 AM
-1import pandas as pd
-2
-3def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-4    return customers.drop_duplicates(subset='email',keep='first')
-5    
+# Last updated: 3/1/2026, 10:12:33 AM
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    return customers.drop_duplicates(subset=["email"], keep="first")
