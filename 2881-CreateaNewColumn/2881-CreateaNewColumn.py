86770a766b15d3f5f6cd7e2529a2d64ce544d7d5
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:33:03 +0100

    File updated at 3/1/2026, 10:33:03 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,8 +1,6 @@
-# Last updated: 3/1/2026, 10:24:51 AM
+# Last updated: 3/1/2026, 10:33:03 AM
 1import pandas as pd
 2
-3def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
-4    col_dict= {'id':'student_id', 'first':'first_name','last':'last_name','age':'age_in_years'}
-5    students = students.rename(columns=col_dict)
-6    return students
-7    
+3def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+4    students = students.astype({'grade':'int'})
+5    return students
