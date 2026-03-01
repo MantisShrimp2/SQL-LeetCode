2b216a053e416107ee9e767cfd9ecf9a69a952f0
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:45:28 +0100

    File updated at 3/1/2026, 10:45:27 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,6 +1,6 @@
-# Last updated: 3/1/2026, 10:37:16 AM
+# Last updated: 3/1/2026, 10:45:27 AM
 1import pandas as pd
 2
-3def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-4    conc = pd.concat([df1,df2])
-5    return conc
+3def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+4    return weather.pivot(index='month', columns='city')['temperature']
+5    
