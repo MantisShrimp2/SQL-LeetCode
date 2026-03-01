b7fe9c9bba5d4baef6453acbeff039f0aaa54489
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:33:29 +0100

    File updated at 3/1/2026, 10:33:29 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,6 +1,6 @@
-# Last updated: 3/1/2026, 10:33:03 AM
-1import pandas as pd
-2
-3def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-4    students = students.astype({'grade':'int'})
-5    return students
+# Last updated: 3/1/2026, 10:33:29 AM
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students['grade'] = students['grade'].astype(int)
+    return students
