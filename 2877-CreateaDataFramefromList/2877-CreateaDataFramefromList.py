f4c32879c087100e68f1a8fcbe66750756115829
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 09:54:30 +0100

    File updated at 3/1/2026, 9:54:30 AM

--- 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
+++ 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
@@ -1,7 +1,5 @@
-# Last updated: 3/1/2026, 9:43:57 AM
+# Last updated: 3/1/2026, 9:54:30 AM
 1import pandas as pd
 2
-3def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-4
-5    df = pd.DataFrame(data=student_data, columns=['student_id','age'])
-6    return df
+3def getDataframeSize(players: pd.DataFrame) -> List[int]:
+4    return [players.shape[0], players.shape[1]]
