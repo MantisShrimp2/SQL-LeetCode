52f55993ebd7bc3117da28386cc9093eb55f2c1a
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 09:56:15 +0100

    File updated at 3/1/2026, 9:56:15 AM

--- 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
+++ 2877-CreateaDataFramefromList/2877-CreateaDataFramefromList.py
@@ -1,7 +1,5 @@
-# Last updated: 3/1/2026, 9:55:30 AM
-1import pandas as pd
-2
-3def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-4    rows = 3
-5    return employees[0:rows]
-6    
+# Last updated: 3/1/2026, 9:56:15 AM
+import pandas as pd
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees.iloc[0:3,:]
