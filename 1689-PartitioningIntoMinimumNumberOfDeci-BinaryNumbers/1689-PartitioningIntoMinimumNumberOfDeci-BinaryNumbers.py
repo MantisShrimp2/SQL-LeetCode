31d8fff28b8d75999256fef11cfa5619a5c26074
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 16:50:56 +0100

    Create 1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers

---
+++ 1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers/1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.py
@@ -0,0 +1,9 @@
+# Last updated: 3/1/2026, 4:50:56 PM
+1class Solution(object):
+2    def minPartitions(self, n):
+3        """
+4        :type n: str
+5        :rtype: int
+6        """ 
+7        return int(max(n))
+8        
