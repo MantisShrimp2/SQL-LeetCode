1767478254cd209e49fa135c8fd38fa1c99811a3
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 16:39:38 +0100

    File updated at 3/1/2026, 4:39:37 PM

--- 205-IsomorphicStrings/205-IsomorphicStrings.py
+++ 205-IsomorphicStrings/205-IsomorphicStrings.py
@@ -1,10 +1,12 @@
-# Last updated: 3/1/2026, 4:37:02 PM
+# Last updated: 3/1/2026, 4:39:37 PM
 1class Solution(object):
-2    def isIsomorphic(self, s, t):
-3        """
-4        :type s: str
-5        :type t: str
-6        :rtype: bool
-7        """
-8        zipped = set(zip(s,t))
-9        return len(zipped) == len(set(s)) == len(set(t))
+2    def wordPattern(self, pattern, s):
+3        arr = s.split()
+4        if len(arr) != len(pattern):
+5            return False
+6        
+7        for i in range(len(arr)):
+8            if pattern.find(pattern[i]) != arr.index(arr[i]):
+9                return False
+10        return True
+11        
