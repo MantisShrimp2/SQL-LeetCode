061527a6ea519c6043679c3dd71297d95702beb4
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:53:14 +0100

    File updated at 3/1/2026, 10:53:13 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,5 +1,6 @@
-# Last updated: 3/1/2026, 10:48:46 AM
+# Last updated: 3/1/2026, 10:53:13 AM
 1import pandas as pd
 2
-3def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-4   return pd.melt(report, id_vars=['product'], var_name='quarter', value_name='sales')
+3def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+4    heavy = animals[animals['weight'] >= 100].sort_values('weight',ascending=False)[['name']]
+5    return heavy
