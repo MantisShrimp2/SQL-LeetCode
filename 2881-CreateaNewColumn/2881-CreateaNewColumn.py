9aec1f73dce3f526e32eab9e4f271bba665b600b
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-01 10:12:04 +0100

    File updated at 3/1/2026, 10:12:04 AM

--- 2881-CreateaNewColumn/2881-CreateaNewColumn.py
+++ 2881-CreateaNewColumn/2881-CreateaNewColumn.py
@@ -1,8 +1,6 @@
-# Last updated: 3/1/2026, 10:05:36 AM
+# Last updated: 3/1/2026, 10:12:04 AM
 1import pandas as pd
 2
-3def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-4    bon_val = 2*employees['salary']
-5
-6    employees.insert(loc=2, column='bonus',value =bon_val)
-7    return employees
+3def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+4    return customers.drop_duplicates(subset='email',keep='first')
+5    
