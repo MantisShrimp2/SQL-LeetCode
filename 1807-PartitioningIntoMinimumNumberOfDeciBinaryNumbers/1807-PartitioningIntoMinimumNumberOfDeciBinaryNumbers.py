be2427def99a5deb2d7a707de7d01af6100534f8
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-03 09:26:58 +0100

    Create 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers

---
+++ 1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers/1807-PartitioningIntoMinimumNumberOfDeciBinaryNumbers.py
@@ -0,0 +1,9 @@
+# Last updated: 3/3/2026, 9:26:58 AM
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """ 
+        return int(max(n))
+        
