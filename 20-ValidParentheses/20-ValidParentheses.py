1633be1bcb109bbe8622e28726963745cd2da372
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-05 16:16:00 +0100

    File updated at 3/5/2026, 4:16:00 PM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,21 +1,24 @@
-# Last updated: 3/5/2026, 4:13:42 PM
-1class Solution(object):
-2    def isValid(self, s):
-3        """
-4        :type s: str
-5        :rtype: bool
-6        """
-7        stack = []
-8
-9        bracket = {'(':')', '[':']','{':"}"}
-10        
-11
-12        for char in s:
-13            if char in bracket:
-14                stack.append(char)
-15            elif len(stack) == 0 or bracket[stack.pop()] != char:
-16                return False
-17        return len(stack) == 0
-18            
-19            
-20    
+# Last updated: 3/5/2026, 4:16:00 PM
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        
+        pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        } # it will compile from left to right then now 
+        
+        for ch in s:# push to stack 
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                
+                top = stack.pop()#storing the top bracket so that we can compare it with thecurrent closing bracket 
+                
+                if pairs[ch] != top: 
+                    return False
+        
+        return len(stack) == 0
