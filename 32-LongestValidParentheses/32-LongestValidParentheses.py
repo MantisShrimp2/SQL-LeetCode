29f97ba532e21b7238850a2f3fd887b196a90768
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-06 11:58:07 +0100

    File updated at 3/6/2026, 11:58:07 AM

--- 32-LongestValidParentheses/32-LongestValidParentheses.py
+++ 32-LongestValidParentheses/32-LongestValidParentheses.py
@@ -1,123 +1,42 @@
-# Last updated: 3/6/2026, 11:14:45 AM
-class Solution(object):
-    def longestValidParentheses(self, s):
-        n=len(s)
-        re=0
-        stack=[-1]
-        for i in range(n):
-            if s[i]=="(":
-                stack.append(i)
-            else:
-                stack.pop()
-                if not stack:
-                    stack.append(i)
-                re=max(re,i-stack[-1])
-        return re
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-                       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Last updated: 3/6/2026, 11:58:07 AM
+1class LRUCache(object):
+2
+3    def __init__(self, capacity):
+4        """
+5        :type capacity: int
+6        """
+7        self.capacity = capacity
+8        self.values = OrderedDict()
+9        
+10
+11    def get(self, key):
+12        """
+13        :type key: int
+14        :rtype: int
+15        """
+16        if key not in self.values:
+17            return -1
+18        else:
+19            self.values[key] = self.values.pop(key)
+20            return self.values[key]
+21
+22    def put(self, key, value):
+23        """
+24        :type key: int
+25        :type value: int
+26        :rtype: None
+27        """
+28        if key not in self.values:
+29            if len(self.values) == self.capacity:
+30                self.values.popitem(last=False)
+31        else:
+32                self.values.pop(key)
+33        self.values[key] = value
+34
+35        
+36
+37
+38# Your LRUCache object will be instantiated and called as such:
+39# obj = LRUCache(capacity)
+40# param_1 = obj.get(key)
+41# obj.put(key,value)
