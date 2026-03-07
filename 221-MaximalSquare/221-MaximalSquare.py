725001e3aca98cec789c521e2ed768249bdbf714
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-07 09:42:30 +0100

    File updated at 3/7/2026, 9:42:30 AM

--- 221-MaximalSquare/221-MaximalSquare.py
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -1,19 +1,19 @@
-# Last updated: 2/26/2026, 2:11:41 PM
-1class Solution(object):
-2    def removeDuplicates(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: int
-6        """
-7        
-8        seen = {}
-9        k = 0
-10
-11        for i in range(len(nums)):
-12            if nums[i] not in seen:
-13                seen[nums[i]] = True
-14                nums[k] = nums[i]
-15                k += 1
-16
-17        return k
-18        
+# Last updated: 3/7/2026, 9:42:30 AM
+1class Solution:
+2    def maximalSquare(self, matrix: List[List[str]]) -> int:
+3    
+4        m, n = len(matrix), len(matrix[0])
+5
+6        dp = [[0] * n for _ in range(m)] # mxn dynamic matrix
+7
+8        max_size = 0
+9
+10        for i in range(m):
+11            for j in range(n):
+12                if matrix[i][j] == '1':
+13                    if i == 0 or j == 0:
+14                        dp[i][j] =1
+15                    else:
+16                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+17                    max_size = max(max_size, dp[i][j])
+18        return max_size*max_size
