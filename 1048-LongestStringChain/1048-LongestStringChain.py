77e335cf912d8d7271fe0466103bdb3965599852
Karan Kumar <60236719+MantisShrimp2@users.noreply.github.com>
2026-03-09 10:03:46 +0100

    Create 1048-LongestStringChain

---
+++ 1048-LongestStringChain/1048-LongestStringChain.py
@@ -0,0 +1,14 @@
+# Last updated: 3/9/2026, 10:03:45 AM
+1class Solution:
+2    def longestStrChain(self, words: List[str]) -> int:
+3        words.sort(key=len)
+4        dp = {}
+5        max_chain = 0
+6        for word in words:
+7            dp[word] = 1
+8            for i in range(len(word)):
+9                prev_word = word[:i] + word[i+1:]
+10                if prev_word in dp:
+11                    dp[word] = max(dp[word], dp[prev_word] + 1)
+12            max_chain = max(max_chain, dp[word])
+13        return max_chain
